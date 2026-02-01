fd6e80eb8ae65322adfdfb41510bb61b04f9ba62
salahuddin212 <salahuddinbayassi@gmail.com>
2026-02-01 13:34:52 +0300

    added uploaded images feature

--- core/views.py
+++ core/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .models import Profile
+from .models import Profile, Post
 from django.contrib.auth.decorators import login_required
 
 
 @login_required(login_url="signin")
 def index(request):
-    return render(request, "index.html")
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    return render(request, "index.html", {"user_profile": user_profile})
 
 
 @login_required(login_url="signin")
@@ -101,3 +103,15 @@ def signin(request):
 def logout(request):
     auth.logout(request)
     return redirect("signin")
+
+
+def upload(request):
+    if request.method == "POST":
+        user = request.user.username
+        image = request.FILES.get("image_upload")
+        caption = request.POST.get("caption")
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+        return redirect("/")
+    else:
+        return redirect("/")
